f4a7590bd97333e13870259b947404b89747fd90
luanOliveira-prog <luan.oliveira242@etec.sp.gov.br>
2024-11-05 21:55:03 -0300

    Add recursos ao app

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,12 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Ubuntu-Bold.ttf", "UbuntuBold");
+                    fonts.AddFont("Ubuntu-Regular.ttf", "Ubuntu");
+                    fonts.AddFont("Ubuntu-Light.ttf", "UbuntuLight");
+                    fonts.AddFont("Caveat-Bold.ttf", "CaveatBold");
+                    fonts.AddFont("Caveat-SemiBold.ttf", "CaveatSemiBold");
+                    fonts.AddFont("Caveat-Regular.ttf", "Caveat");
                 });
 
 #if DEBUG
---
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiAppHotel.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+	public ContratacaoHospedagem()
+	{
+		InitializeComponent();
+	}
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+}
